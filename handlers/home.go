7ef68bae0b835d6f4c6dc4db79c2f580aaa3788b
Edigiraldo <edison.giraldoa12@gmail.com>
2022-08-10 19:27:15 -0500

    handlers for signup, loging and get user info

--- handlers/home.go
+++ handlers/home.go
@@ -3,8 +3,12 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
+	"github.com/Edigiraldo/RestWebSockets/models"
+	"github.com/Edigiraldo/RestWebSockets/repository"
 	"github.com/Edigiraldo/RestWebSockets/server"
+	"github.com/golang-jwt/jwt"
 )
 
 type HomeResponse struct {
@@ -23,5 +27,32 @@ func HomeHandler(s server.Server) http.HandlerFunc {
 				Status:  200,
 			},
 		)
+	}
+}
+
+func MeHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenString := strings.TrimSpace(r.Header.Get("Authorization"))
+		token, err := jwt.ParseWithClaims(tokenString, &models.AppClaims{},
+			func(token *jwt.Token) (interface{}, error) {
+				return []byte(s.Config().JWTSecret), nil
+			})
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+		}
+
+		if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
+			user, err := repository.GetUserById(r.Context(), claims.UserId)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
---
+++ handlers/user.go
@@ -0,0 +1,124 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Edigiraldo/RestWebSockets/models"
+	"github.com/Edigiraldo/RestWebSockets/repository"
+	"github.com/Edigiraldo/RestWebSockets/server"
+	"github.com/golang-jwt/jwt"
+	"github.com/segmentio/ksuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type SingUpLoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type SingUpResponse struct {
+	Id    string `json:"id"`
+	Email string `json:"email"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
+func SingUpHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		request := SingUpLoginRequest{}
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		id, err := ksuid.NewRandom()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var user = models.User{
+			ID:       id.String(),
+			Email:    request.Email,
+			Password: string(hashedPassword),
+		}
+
+		err = repository.InsertUser(r.Context(), &user)
+		if err != nil {
+			log.Printf("Error inserting user: %v", err)
+			http.Error(w, "there was an error while creating user", http.StatusInternalServerError)
+			return
+		}
+
+		response := SingUpResponse{
+			Id:    id.String(),
+			Email: request.Email,
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+func LoginHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		request := SingUpLoginRequest{}
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		log.Println(request)
+
+		user, err := repository.GetUserByEmail(r.Context(), request.Email)
+		if err != nil {
+			http.Error(w, "there was an error while logging user", http.StatusInternalServerError)
+			return
+		}
+
+		if user == nil {
+			http.Error(w, "invalid credentials", http.StatusUnauthorized)
+			return
+		}
+
+		err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Password))
+		if err != nil {
+			http.Error(w, "invalid credentials", http.StatusUnauthorized)
+			return
+		}
+
+		claims := models.AppClaims{
+			UserId: user.ID,
+			StandardClaims: jwt.StandardClaims{
+				ExpiresAt: time.Now().Add(2 * time.Hour * 24).Unix(),
+			},
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		tokenString, err := token.SignedString([]byte(s.Config().JWTSecret))
+		if err != nil {
+			http.Error(w, "there was an error while logging user", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(LoginResponse{
+			Token: tokenString,
+		})
+	}
+}
